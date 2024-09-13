8e0f3a2442f8abb4e7df6559552a18cf9f2efa15
devansh-kc <devansh.kc96@gmail.com>
2024-09-13 17:43:18 +0530

    fixed error

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,15 +1,25 @@
 import bcrypt from "bcryptjs";
+import prisma from "../lib/prisma.js";
 async function login(req, res) {
   res.send("login page");
 }
 async function logout(params) {}
 async function register(req, res) {
   const { username, email, password } = req.body;
-  if (username &&  email && password === " ") {
+  if (username && email && password === " ") {
     return res.status(400).json("we need all the para meters");
   }
-  const hashedPassword =  await bcrypt.hash(password,9);
-  return res.status(200).json({ username, email, hashedPassword });
+  const hashedPassword = await bcrypt.hash(password, 9);
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password:hashedPassword,
+    },
+  });
+  console.log(newUser)
+
+  return res.status(200).json(newUser);
 }
 
 export { login, logout, register };
