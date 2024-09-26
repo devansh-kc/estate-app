ef4d75ccf52a2e7d0a3ad9e63d8dd71d8b0042aa
devansh-kc <devansh.kc96@gmail.com>
2024-09-26 17:50:46 +0530

    added list page and cards in list page

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./card.scss"
-function Cart({item}) {
+import "./card.scss";
+import { MapPin } from "lucide-react";
+function Cart({ item }) {
   return (
     <div className="card">
       <Link to={`/${item.id}`} className="imageContainer">
         <img src={item.img} alt={item.title} />
       </Link>
-      <div className="textContainer"></div>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={item.id}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <MapPin />
+        </p>
+      </div>
     </div>
   );
 }
