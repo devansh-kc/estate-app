eec7eeab631f9757234b1a66ef699e0a6ca3f9a1
devansh-kc <devansh.kc96@gmail.com>
2024-09-27 22:14:29 +0530

    Worked on data scroll and i worked on cards

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./card.scss";
-import { MapPin } from "lucide-react";
+import {
+  BathIcon,
+  BedDouble,
+  Bookmark,
+  DollarSign,
+  MapPin,
+  MessageCircle,
+  Save,
+} from "lucide-react";
 function Cart({ item }) {
   return (
     <div className="card">
@@ -13,8 +21,34 @@ function Cart({ item }) {
           <Link to={item.id}>{item.title}</Link>
         </h2>
         <p className="address">
-          <MapPin />
+          <MapPin className="lucide-icons" />
+          <span>{item.address}</span>
         </p>
+
+        <p className="price">
+          <DollarSign className="lucide-icons" />
+          <span>{item.price}</span>
+        </p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <BedDouble className="lucide-icons" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <BathIcon className="lucide-icons" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <Bookmark className="lucide-icons" />
+            </div>
+            <div className="icon">
+              <MessageCircle className="lucide-icons" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
