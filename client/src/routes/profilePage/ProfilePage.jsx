6c9ead93bb56d67d16ffa6c5fd8c4eb49bb256e8
devansh-kc <devansh.kc96@gmail.com>
2024-10-07 17:36:37 +0530

    made chat component and list component

--- client/src/routes/profilePage/ProfilePage.jsx
+++ client/src/routes/profilePage/ProfilePage.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import "./profilePage.scss"
+import List from "../../components/List/List";
+import Chat from "../../components/chat/Chat";
 
 function ProfilePage() {
-  return (
-    <div className="profilePage">
-      <div className="userInfo">
-        {/* name, username, email, image,  */}
-        <p>name:John doe</p>
-        <p>username :JohnDoe</p>
-        <p>email:john Doe</p>
-        <p>user img</p>
-        <img
-          style={{ width: "100px", height: "100px", borderRadius:"50%" }}
-          src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="username"
-        />
+    return (
+      <div className="profilePage">
+        <div className="details">
+          <div className="wrapper">
+            <div className="title">
+              <h1>User Information</h1>
+              <button>Update Profile</button>
+            </div>
+            <div className="info">
+              <span>
+                Avatar:
+                <img
+                  src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  alt=""
+                />
+              </span>
+              <span>
+                Username: <b>John Doe</b>
+              </span>
+              <span>
+                E-mail: <b>john@gmail.com</b>
+              </span>
+            </div>
+            <div className="title">
+              <h1>My List</h1>
+              <button>Create New Post</button>
+            </div>
+            <List />
+            <div className="title">
+              <h1>Saved List</h1>
+            </div>
+            <List />
+          </div>
+        </div>
+        <div className="chatContainer">
+          <div className="wrapper">
+            <Chat/>
+          </div>
+        </div>
       </div>
-      <div className="messages"></div>
-      <div className="userWhistlist"></div>
-    </div>
-  );
+    );
+  
 }
 
 export default ProfilePage;
